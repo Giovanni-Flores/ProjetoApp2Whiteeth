a02b665eba7858e24e424c62bf66b8bb29e6eb1a
Giovanni-Flores <66764596+Giovanni-Flores@users.noreply.github.com>
2021-07-14 19:33:27 -0300

    Projeto Finalizado

---
+++ projetowhiteeth/android/app/src/main/kotlin/com/projetowhiteeth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.projetowhiteeth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ projetowhiteeth/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'About.dart';
+import 'Gallery.dart';
+
+void main() {
+  runApp(MaterialApp(
+    //home: Gallery(),
+    home: About(),
+  ));
+}
